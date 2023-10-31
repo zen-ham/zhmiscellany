31ffcbf913ae0cba2ba0a6a40d91b0739a90617a
Your Name <youremail@example.com>
2023-11-01 08:16:22 +1100

    Automated commit from zh at 2023-11-01 08:16:22.446880

--- src/zhmiscellany/image.py
+++ src/zhmiscellany/image.py
@@ -2,6 +2,9 @@ import numpy as np
 
 
 def image_diff(img1, img2):
+    img1 = img1.resize((100, 100))
+    img2 = img2.resize((100, 100))
+
     # Convert images to numpy arrays
     img_arr1 = np.array(img1)
     img_arr2 = np.array(img2)
