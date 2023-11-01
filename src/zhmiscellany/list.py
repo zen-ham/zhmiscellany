c0e65f92f158fdf6c8f85ad902d892ed7c427b3b
Your Name <youremail@example.com>
2023-11-02 01:17:57 +1100

    Automated commit from zh at 2023-11-02 01:17:57.530661

--- src/zhmiscellany/list.py
+++ src/zhmiscellany/list.py
@@ -2,11 +2,12 @@ def remove_duplicates(l_list):
     return list(dict.fromkeys(l_list))
 
 
-def subtract_lists(lists):
-    l_list = []
-    for i in lists:
-        l_list.extend(i)
-    return remove_duplicates(l_list)
+def subtract_lists(main_list, *other_lists):
+    # Create a set of all items in other lists
+    other_items = set(item for sublist in other_lists for item in sublist)
+
+    # Return items from the main list that are not in other lists
+    return [item for item in main_list if item not in other_items]
 
 
 def remove_duplicates_by_element(tuple_list, element):
