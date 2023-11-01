0616f4cbc06e163608cf57ff1cb5c57db6fe4ad3
Your Name <youremail@example.com>
2023-11-02 03:44:36 +1100

    Automated commit from zh at 2023-11-02 03:44:36.590330

--- src/zhmiscellany/_supportfuncs.py
+++ src/zhmiscellany/_supportfuncs.py
@@ -139,19 +139,31 @@ def print_str_if(string, print_it):
 
 
 def set_tesseract_path():
+    anyway = False
     if getattr(sys, 'frozen', False):
         # we are running in a PyInstaller bundle
         base_path = sys._MEIPASS
     else:
         # we are running in normal Python environment
         base_path = os.path.dirname(__file__)
+        state_path = os.path.join(base_path, '_state.py')
+        if os.path.exists(state_path):
+            with open(state_path, 'r') as f:
+                state = f.read()
+            if state == '_state=0':
+                with open(state_path, 'w') as f:
+                    f.write('_state=1')
+                anyway = True
+
     cwd = os.getcwd()
-    if not os.path.exists(os.path.join(base_path, 'resources')):
+    if (not os.path.exists(os.path.join(base_path, 'resources'))) or anyway:
+        if os.path.exists(os.path.join(base_path, 'resources')):
+            zhmiscellany.fileio.remove_folder(os.path.join(base_path, 'resources'))
         os.chdir(base_path)
         from ._py_resources import gen
         gen()
         os.chdir(cwd)
-    tesseract_path = pkg_resources.resource_filename(base_path, 'resources/tesseract')
+    tesseract_path = os.path.join(base_path, 'resources', 'tesseract')
     pytesseract.pytesseract.tesseract_cmd = f'{tesseract_path}\\tesseract.exe'
 
 
