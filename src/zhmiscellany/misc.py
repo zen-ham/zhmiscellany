b501ef55ee3db967606a997d2ae32de38d2fdfee
Your Name <youremail@example.com>
2023-11-14 19:46:36 +1100

    Automated commit from zh at 2023-11-14 19:46:36.610708

--- src/zhmiscellany/misc.py
+++ src/zhmiscellany/misc.py
@@ -1,6 +1,13 @@
 import os, signal
 from ._misc_supportfuncs import set_activity_timeout, activity
+import zhmiscellany.math
 
 
 def die():
     os.kill(os.getpid(), signal.SIGTERM)
+
+
+def show_progress(things, total_things):
+    print(f'\r{zhmiscellany.math.smart_percentage(things, total_things)}%', end='')
+    if things == total_things:
+        print('')
