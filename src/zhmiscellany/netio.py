0bef1292aca4ada1b4703ec969e2fcb253dc0779
Your Name <youremail@example.com>
2023-11-17 02:38:43 +1100

    Automated commit from zh at 2023-11-17 02:38:42.989092

--- src/zhmiscellany/netio.py
+++ src/zhmiscellany/netio.py
@@ -3,14 +3,19 @@ import zhmiscellany.string
 import urllib.parse
 
 
+def resolve_file(url, destination_folder="."):
+    file_name = urllib.parse.unquote(url.split("/")[-1])
+    destination_path = f"{destination_folder}/{file_name}"
+    if len(destination_path) > 250:
+        destination_path = f'{destination_path[:(250 - len(os.path.splitext(destination_path)[1]))]}{os.path.splitext(destination_path)[1]}'
+    return destination_path
+
+
 def download_file(url, destination_folder="."):
     response = requests.get(url, stream=True)
     if response.status_code == 200:
         # Get the file name from the URL
-        file_name = urllib.parse.unquote(url.split("/")[-1])
-        destination_path = f"{destination_folder}/{file_name}"
-        if len(destination_path) > 250:
-            destination_path = f'{destination_path[:(250-len(os.path.splitext(destination_path)[1]))]}{os.path.splitext(destination_path)[1]}'
+        destination_path = resolve_file(url, destination_folder)
 
         if os.path.exists(destination_path):
             return False
