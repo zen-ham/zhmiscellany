2c81085765f5d132742ddc7cffa8283133658e5f
Your Name <youremail@example.com>
2024-02-13 02:03:24 +1100

    Automated commit from zh at 2024-02-13 02:03:24.107838

--- src/zhmiscellany/processing.py
+++ src/zhmiscellany/processing.py
@@ -1,4 +1,5 @@
 import threading
+import traceback
 import zhmiscellany.string
 import concurrent.futures
 
@@ -12,8 +13,10 @@ def start_daemon(**kwargs):
 
 def batch_threading(targets, max_threads):
     def execute_target(target):
-        target[0](*target[1])
-
+        try:
+            target[0](*target[1])
+        except Exception as e:
+            print(traceback.format_exc())
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
         futures = {executor.submit(execute_target, target): target for target in targets}
 
