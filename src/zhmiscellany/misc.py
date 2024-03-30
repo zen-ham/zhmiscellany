7135ad158ae40700a055c1e4faf10fa094e5cd05
Your Name <youremail@example.com>
2024-03-31 05:10:34 +1100

    Automated commit from zh at 2024-03-31 05:10:34.837891

--- src/zhmiscellany/misc.py
+++ src/zhmiscellany/misc.py
@@ -122,3 +122,16 @@ def get_mouse_xy():
 
 def base62_hash(__obj):
     return zhmiscellany.string.convert_to_base62(hash(__obj))
+
+
+def high_precision_sleep(duration):
+    start_time = time.perf_counter()
+    while True:
+        elapsed_time = time.perf_counter() - start_time
+        remaining_time = duration - elapsed_time
+        if remaining_time <= 0:
+            break
+        if remaining_time > 0.02:  # Sleep for 5ms if remaining time is greater
+            time.sleep(max(remaining_time/2, 0.0001))  # Sleep for the remaining time or minimum sleep interval
+        else:
+            pass
