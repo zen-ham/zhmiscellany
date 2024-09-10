db9c665b6ec08d3bc181fb54b19dee8633d22dd0
Your Name <again.really.plz@gmail.com>
2024-09-11 01:30:33 +1000

    Automated commit from zen-ham at 2024-09-11 01:30:33.824964

--- src/zhmiscellany/math.py
+++ src/zhmiscellany/math.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 def smart_percentage(things, total_things):
@@ -21,3 +22,14 @@ def calculate_evenly_spaced_points(duration, segments, offset=0):
 
 def clamp(value, minimum, maximum):
     return max(min(value, maximum), minimum)
+
+
+def generate_grid(top_left, bottom_right, rows, cols, int_coords=True):
+    x = np.linspace(top_left[0], bottom_right[0], cols)
+    y = np.linspace(top_left[1], bottom_right[1], rows)
+    grid = np.array(np.meshgrid(x, y)).T.reshape(-1, 2)
+
+    if int_coords:
+        grid = np.rint(grid).astype(int)
+
+    return [tuple(coord) for coord in grid]
