1423420b3939a0e97148b9e4d77b9383fbdf85b2
zen-ham <again.really.plz@gmail.com>
2025-03-12 04:48:05 -0600

    Automated commit from zen-ham at 2025-03-12 04:48:05.504220

--- src/zhmiscellany/rust.py
+++ src/zhmiscellany/rust.py
@@ -1,19 +1,22 @@
-from zhmiscellanyrusteffect import *
+import zhmiscellanyrusteffect
 import numpy as np
 
-np_mean = np_mean
-np_sum = np_sum
-np_median = np_median
-list_files_recursive = list_files_recursive
+list_files_recursive = zhmiscellanyrusteffect.list_files_recursive
+def numpy_mean(np_arr):
+    return zhmiscellanyrusteffect.np_mean(np_arr.astype(np.float64, copy=False))
+def numpy_sum(np_arr):
+    return zhmiscellanyrusteffect.np_sum(np_arr.astype(np.float64, copy=False))
+def numpy_median(np_arr):
+    return zhmiscellanyrusteffect.np_median(np_arr.astype(np.float64, copy=False))
 
 def mean(lst):
-    array = np.array(lst)
+    array = np.array(lst, dtype=np.float64)
     return np_mean(array)
 
 def sum(lst):
-    array = np.array(lst)
+    array = np.array(lst, dtype=np.float64)
     return np_sum(array)
 
 def median(lst):
-    array = np.array(lst)
+    array = np.array(lst, dtype=np.float64)
     return np_median(array)
