e3e083dbf817e0599551977116a0e89fdac43c61
zen-ham <again.really.plz@gmail.com>
2025-04-01 01:41:11 -0600

    Automated commit from zen-ham at 2025-04-01 01:41:11.142924

--- src/zhmiscellany/cpp.py
+++ src/zhmiscellany/cpp.py
@@ -1,1 +1,4 @@
-from fast_array_diff import remove_items as subtract_lists
+try:
+    from fast_array_diff import remove_items as subtract_lists
+except:
+    pass # bruh, idk why this happens on some machines tbh and i aint tryna fix this rn it's 1:40 am
